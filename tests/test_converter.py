d5a8492f693dde1ad4a996c694a61f8f0b427cbb
v3nd3774 <josue.caraballo@gmail.com>
2019-03-15 15:38:27 -0500

    made ungoro adaptation test fail now

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -46,4 +46,52 @@ def test_leeroy():
       0, #pirate
       0, #totem
     ])
-  ).all
+  ).all()
+
+def test_verdant_longneck():
+  card_id = "UNG_100"
+  assert (
+    Converter.convert(card_id) == np.array([
+      5, #attack
+      4, #hp
+      5, #cost
+      1, #adapt
+      1, #battlecry
+      0, #casts when drawn
+      0, #charge
+      0, #choose one
+      0, #choose twice
+      0, #combo
+      0, #counter
+      0, #deathrattle
+      0, #discover
+      0, #divine shield
+      0, #echo
+      0, #freeze
+      0, #immune
+      0, #inspire
+      0, #lifesteal
+      0, #magnetic
+      0, #mega-windfury
+      0, #overkill
+      0, #overload
+      0, #passive
+      0, #poisonous
+      0, #quest
+      0, #recruit
+      0, #rush
+      0, #secret
+      0, #silence
+      0, #start of game
+      0, #taunt
+      0, #windfury
+      1, #beast
+      0, #demon
+      0, #dragon
+      0, #elemental
+      0, #mech
+      0, #murloc
+      0, #pirate
+      0, #totem
+    ])
+  ).all()
