9e6ff766878f85cd8334675a82eead02351d1d1a
v3nd3774 <josue.caraballo@gmail.com>
2019-03-15 16:36:49 -0500

    failing test on counterspell

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -239,3 +239,51 @@ def test_si7_agent():
       0, #totem
     ])
   ).all()
+
+def test_counterspell():
+  card_id = "EX1_287"
+  assert (
+    Converter.convert(card_id) == np.array([
+      0, #attack
+      0, #hp
+      3, #cost
+      0, #adapt
+      0, #battlecry
+      0, #casts when drawn
+      0, #charge
+      0, #choose one
+      0, #choose twice
+      0, #combo
+      1, #counter
+      0, #deathrattle
+      0, #discover
+      0, #divine shield
+      0, #echo
+      0, #freeze
+      0, #immune
+      0, #inspire
+      0, #lifesteal
+      0, #magnetic
+      0, #mega-windfury
+      0, #overkill
+      0, #overload
+      0, #passive
+      0, #poisonous
+      0, #quest
+      0, #recruit
+      0, #rush
+      1, #secret
+      0, #silence
+      0, #start of game
+      0, #taunt
+      0, #windfury
+      0, #beast
+      0, #demon
+      0, #dragon
+      0, #elemental
+      0, #mech
+      0, #murloc
+      0, #pirate
+      0, #totem
+    ])
+  ).all()
