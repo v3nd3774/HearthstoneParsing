95aa89130adfbc4700780b01dd6950a7fcd8077e
v3nd3774 <josue.caraballo@gmail.com>
2019-03-15 12:58:38 -0500

    trying to fix unit test

---
+++ converter/__init__.py
@@ -0,0 +1,1 @@
+name = "converter_pkg"
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as f:
+  long_description = f.read()
+setuptools.setup(
+  name="hearthstone-parsing-converter-v3nd3774",
+  version="0.0.1",
+  author="Josue Caraballo",
+  author_email="josue.caraballo@gmail.com",
+  description="Converting hearthstone entity strings into fixed length vectors!",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/v3nd3774/HearthstoneParsing",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: Developed on Windows 10",
+  ]
+)
