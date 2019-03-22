305729d44312689f50506efe07d8d2a9b5e7e032
v3nd3774 <josue.caraballo@gmail.com>
2019-03-21 22:19:10 -0500

    uploaded to test pypi

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -5,4 +5,4 @@ old_num=`perl -ne "print if s/.+version=\"\d\.\d\.(\d)\".+/\1/g" setup.py`
 export new_num=`expr $old_num + 1`
 perl -i'' -pe 's/(.*?version=\"\d\.\d\.)\d(\"..*?)/\1$ENV{new_num}\2/g' setup.py
 python setup.py sdist bdist_wheel
-twine upload -r testpypi dist/*
+twine upload -r testpypi dist/* -u $1 -p $2
